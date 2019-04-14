17d992c8b66aeb8b20195facb309331531b5e034
sebastian kovacs work <sebastian.kovacs@fortech.ro>
2019-04-14 16:23:43 +0300

    update

--- src/services/apiService.js
+++ src/services/apiService.js
@@ -1,7 +1,7 @@
 import fetchWrapper from "../helpers/fetchWrapper";
 // import responseHelper from "./responseHelper";
 
-const baseUrl = process.NODE_ENV === "production" ? "http://reservr-staging.reservr.net/api" : "http://localhost:8080";
+const baseUrl = process.envNODE_ENV === "production" ? "http://reservr-staging.reservr.net/api" : "http://localhost:8080";
 
 const checkOrgNameAvailability = ( orgname ) => {
     const url = `${ baseUrl }/orgs?slug=${ orgname }`;
